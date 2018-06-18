efee1fbf2f46735b6674f0f35f41d7846e833f95
Juhanj <juhani.jarvio@gmail.com>
2018-06-18 19:55:29 +0300

    Changes too numerous to mention

--- psoboidswarm.py
+++ psoboidswarm.py
@@ -9,12 +9,12 @@ def main() :
     size = 800, 600
     screen = pygame.display.set_mode(size)
 
-    X_LIM = 10
-    Y_LIM = 10
+    X_LIM = [-100,100]
+    Y_LIM = [-100,100]
     swarm = Swarm( 6, X_LIM, Y_LIM )
 
-    for i in range(0,100) :
-        drawSwarm( swarm )
+    for i in range(0,10) :
+        swarm.simplePrintSwarm()
         swarm.updateSwarm()
         t.sleep(0.5)
         
@@ -41,19 +41,17 @@ def main() :
     return;
 
 def drawSwarm( swarm ) :
-    for boid in swarm.list :
-        print( np.round(boid.position,0), end='' )
-    print('')
     return;
 
 class Swarm :
     def __init__(self, sizeOfFlock, xAxis, yAxis):
-        self.sizeOfFlock = sizeOfFlock
+        self.sizeOfFlock = sizeOfFlock # For calculating averages
+        self.bounds = [xAxis, yAxis]
         self.list = []
         for i in range( 0, sizeOfFlock ) :
             self.list.append( Boid(
-                random.randint(0,xAxis),
-                random.randint(0,yAxis),
+                random.randint(xAxis[0],xAxis[1]),
+                random.randint(yAxis[0],yAxis[1]),
                 0,
                 self
             ))
@@ -61,16 +59,25 @@ class Swarm :
     def updateSwarm(self) :
 
         for boid in self.list :
-            v1 = boid.rule1_separation() # for rule 1 - separation, avoiding collision
-            v2 = boid.rule2_alignment()  # for rule 2 - alignment, matching velocity
-            v3 = boid.rule3_cohesion()   # for rule 3 - cohesion, staying close to others
+            ## Three main rules
+            v1 = boid.rule1_separation() # separation, avoiding collision
+            v2 = boid.rule2_alignment()  # alignment, matching velocity
+            v3 = boid.rule3_cohesion()   # cohesion, staying close to others
+            ## Extra rules
+            v4 = boid.rule4_bounds() # bounds, boid should stay inside visible boundaries
+            #v5 = boid.rule5_speedLimit() # speed limit, no arbitarily fast speeds
 
-            #boid.velocity = np.array([ boid.velocity, v1, v2, v3 ]).sum(axis=0)
-            boid.velocity = boid.velocity + v1 + v2 + v3
-            #boid.position = np.add( boid.position + boid.velocity )
+            boid.velocity = boid.velocity + v1 + v2 + v3 + v4
             boid.position = boid.position + boid.velocity
 
         return;
+
+    def simplePrintSwarm(self) :
+        for boid in self.list :
+            print( np.round(boid.position,1), end='' )
+        print('')
+        return;
+        
 
 
 class Boid :
@@ -80,15 +87,15 @@ class Boid :
         self.y = y
         # By using np.array() we can use simple x + y operators,
         # instead of np.add(x,y). Makes code more readable.
-        self.velocity = np.array([ self.x, self.y ], dtype=np.float64)
         self.position = np.array([ self.x, self.y ], dtype=np.float64)
+        self.velocity = np.array([ 0, 0 ], dtype=np.float64)
 
     def rule1_separation(self) :
         c = np.array([0,0], dtype=np.float64)
 
         for boid in self.swarm.list :
             if boid != self :
-                if abs(boid.position - self.position).all() < 5 :
+                if abs(boid.position - self.position).all() < 10 :
                     c = c - (boid.position - self.position)
 
         return c;
@@ -97,7 +104,6 @@ class Boid :
         perceivedVelocity = None
 
         for boid in self.swarm.list :
-			print ( boid != self )
             if boid != self :
                 if perceivedVelocity is not None :
                     perceivedVelocity = perceivedVelocity + boid.velocity
@@ -120,8 +126,23 @@ class Boid :
                     perceivedCenter = boid.position
 
         perceivedCenter = perceivedCenter / (self.swarm.sizeOfFlock-1)
-        perceivedCenter = (perceivedCenter - self.position) / 50
+        perceivedCenter = (perceivedCenter - self.position) / 100
 
         return perceivedCenter;
+
+    def rule4_bounds(self) :
+        v = np.array([0,0], dtype=np.float64)
+
+        if self.position[0] < self.swarm.bounds[0][0] :
+            v[0] = 10
+        elif self.position[0] > self.swarm.bounds[0][1] :
+            v[0] = -10
+        
+        if self.position[1] < self.swarm.bounds[1][0] :
+            v[1] = 10
+        elif self.position[1] > self.swarm.bounds[1][1] :
+            v[1] = -10
+            
+        return 0;
 
 main()
